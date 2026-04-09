ab979fd5065a520693aac8722aa5025a3975289e
Anandupy <94069496+Anandupy@users.noreply.github.com>
2026-04-09 13:33:37 +0530

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,1 +1,12 @@
+window.DEN_SUPABASE_CONFIG = {
+  supabaseUrl: "https://pepxczgriutjlziwraax.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlcHhjemdyaXV0amx6aXdyYWF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MTc4NTUsImV4cCI6MjA5MTI5Mzg1NX0.qy6bpWBGl2yd3XXsvskss81d_mvIRMqiXpfe6fOI-tE",
+  gym: {
+    name: "DEN Fitness",
+    address: "Shop Number 2, New Kailas Niwas 2, Near 90 Feet Rd, Netaji Nagar, Ashok Nagar, Saki Naka, Mumbai, Maharashtra 400072",
+    lat: 19.0937,
+    lng: 72.8866,
+    radiusMeters: 200,
+  },
+};
 
